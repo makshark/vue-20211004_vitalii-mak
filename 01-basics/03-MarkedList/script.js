cb99ee029e21a667bf50f235bcbe787c734cd8a0
makshark <vitalii.makovskyi@gmail.com>
2021-10-10 20:30:10 +0300

    03-MarkedList

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,32 +1,46 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// From https://jsonplaceholder.typicode.com/comments
-const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
+const vueApp = createApp({
+  data() {
+    return {
+      emails: [
+        'Eliseo@gardner.biz',
+        'Jayne_Kuhic@sydney.com',
+        'Nikita@garfield.biz',
+        'Lew@alysha.tv',
+        'Hayden@althea.biz',
+        'Presley.Mueller@myrl.com',
+        'Dallas@ole.me',
+        'Mallory_Kunze@marie.org',
+        'Meghan_Littel@rene.us',
+        'Carmen_Keeling@caroline.name',
+        'Veronica_Goodwin@timmothy.net',
+        'Oswald.Vandervort@leanne.org',
+        'Kariane@jadyn.tv',
+        'Nathan@solon.io',
+        'Maynard.Hodkiewicz@roberta.com',
+        'Christine@ayana.info',
+        'Preston_Hudson@blaise.tv',
+        'Vincenza_Klocko@albertha.name',
+        'Madelynn.Gorczany@darion.biz',
+        'Mariana_Orn@preston.org',
+        'Noemie@marques.me',
+        'Khalil@emile.co.uk',
+        'Sophia@arianna.co.uk',
+        'Jeffery@juwan.us',
+        'Isaias_Kuhic@jarrett.net',
+      ],
+      search_input: null
+    }
+  },
+  computed: {
+    filteredEmails() {
+      let search_input = this.search_input;
+      return this.emails.map(function (value, i) {
+        return { value: value, marked: value.includes(search_input) && search_input !== '' }
+      });
+    }
+  }
+})
 
-// Требуется создать Vue приложение
+vueApp.mount('#app')
