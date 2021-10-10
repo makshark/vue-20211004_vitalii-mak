bac2d49f84408d29da5a6a57ca1b02fdb553aa00
makshark <vitalii.makovskyi@gmail.com>
2021-10-10 18:59:16 +0300

    02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vueApp = createApp({
+  data() {
+    return {
+      value1: 0,
+      value2: 0,
+      //possible values [sum, subtract, multiply, divide]
+      operator_btn: 'sum',
+    }
+  },
+  computed: {
+    calculate() {
+      if (this.value2 === 0 && this.operator_btn === 'divide') {
+        return 'zero division is not allowed'
+      }
+      switch(this.operator_btn) {
+        case "sum":
+          return this.value1 + this.value2
+        case "subtract":
+          return this.value1 - this.value2
+        case "multiply":
+          return this.value1 * this.value2
+        case "divide":
+          return this.value1 / this.value2
+        default:
+          return 'something went wrong'
+      }
+    }
+  }
+})
+
+vueApp.mount('#app')
