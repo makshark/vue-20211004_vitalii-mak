cecd289ba1f9a583d254880033b90c106323cb96
makshark <vitalii.makovskyi@gmail.com>
2021-10-14 16:22:17 +0300

    MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,18 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const vueApp = createApp({
+  data() {
+    return {
+      meetupId: null,
+      title: null
+    }
+  },
+  watch: {
+    async meetupId(newValue, oldValue) {
+      const data = await fetchMeetupById(newValue)
+      this.title = data.title;
+    }
+  }
+})
+vueApp.mount('#app')
