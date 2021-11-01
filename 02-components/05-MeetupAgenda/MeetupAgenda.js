bb1f146795c6b102bc58f6c34ef89e5ee2b58eb5
makshark <vitalii.makovskyi@gmail.com>
2021-11-02 01:59:25 +0200

    05-MeetupAgenda

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,26 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
+  components: {
+    MeetupAgendaItem
+  },
+  props: {
+    agenda: {
+      type: Array,
+      required: true
+    }
+  },
 
   template: `
     <ul class="agenda">
       <li class="agenda__item">
+        <p>Info about meetup</p>
         <!-- meetup agenda item -->
+          <div v-for="item in agenda" >
+            <meetup-agenda-item :agendaItem="item"></meetup-agenda-item>
+          </div>
       </li>
     </ul>`,
 });
--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,33 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  agendaItemIcons,
+  agendaItemDefaultTitles,
+
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true
+    }
+  },
+
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="'/assets/icons/icon-' + $options.agendaItemIcons[agendaItem.type] + '.svg'" class="icon" alt="key" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <h3 class="agenda-item__title">{{ agendaItem.title ? agendaItem.title : $options.agendaItemDefaultTitles[agendaItem.type] }}</h3>
+        <p v-if="agendaItem.type === 'talk'" class="agenda-item__talk">
+          <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
         </p>
-        <p>Description</p>
+        <p v-if="agendaItem.description">{{ agendaItem.description }}</p>
       </div>
     </div>`,
 });
