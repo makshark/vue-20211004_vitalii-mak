df1cfb0d2534aafd4ec3c7e229cd9277c07a25ad
makshark <vitalii.makovskyi@gmail.com>
2021-11-02 02:54:40 +0200

    06-MeetupView

--- 02-components/06-MeetupView/MeetupView.js
+++ 02-components/06-MeetupView/MeetupView.js
@@ -1,6 +1,11 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
+import MeetupDescription from '../02-MeetupDescription/MeetupDescription.js';
+import MeetupCover from '../03-MeetupCover/MeetupCover.js';
+import MeetupInfo from '../04-MeetupInfo/MeetupInfo.js';
+import MeetupAgenda from '../05-MeetupAgenda/MeetupAgenda.js';
+import MeetupAgendaItem from '../05-MeetupAgenda/MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupView',
@@ -8,6 +13,16 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    MeetupAgenda,
+    MeetupAgendaItem
+  },
+  computed: {
+    checkEmptyAgenda() {
+      return Array.isArray(this.meetup.agenda) && this.meetup.agenda.length
+    }
   },
 
   props: {
@@ -19,20 +34,20 @@ export default defineComponent({
 
   template: `
     <div>
-      <!-- meetup cover-->
+    <meetup-cover :title="meetup.title" :image="meetup.image"></meetup-cover>
 
       <ui-container>
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
+            <meetup-description :description="meetup.description"></meetup-description>
 
             <h3>Программа</h3>
-            <!-- meetup agenda -->
-            <ui-alert>Программа пока пуста...</ui-alert>
+            <meetup-agenda v-if="checkEmptyAgenda" :agenda="meetup.agenda"></meetup-agenda>
+            <ui-alert v-else>Программа пока пуста...</ui-alert>
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+            <meetup-info :organizer="meetup.organizer" :place="meetup.place" :date="meetup.date" ></meetup-info>
           </div>
         </div>
       </ui-container>
