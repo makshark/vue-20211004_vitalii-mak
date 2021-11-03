82952af638372ccc5f818a389a63da93517aa948
makshark <vitalii.makovskyi@gmail.com>
2021-11-03 08:50:44 +0200

    07-PageMeetup

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,26 +1,67 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupDescription from '../02-MeetupDescription/MeetupDescription.js';
+import MeetupCover from '../03-MeetupCover/MeetupCover.js';
+import MeetupInfo from '../04-MeetupInfo/MeetupInfo.js';
+import MeetupAgenda from '../05-MeetupAgenda/MeetupAgenda.js';
+import MeetupAgendaItem from '../05-MeetupAgenda/MeetupAgendaItem.js';
+import MeetupView from "../06-MeetupView/MeetupView.js";
+import { fetchMeetupById } from './meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
+  props:{
+    meetupId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      meetup: null,
+      errorDuringRequest: null
+    }
+  },
+  mounted() {
+    this.handleRequest(this.meetupId)
+  },
 
   components: {
     UiAlert,
     UiContainer,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    MeetupAgenda,
+    MeetupAgendaItem,
+    MeetupView,
   },
+  watch: {
+    meetupId(newValue, oldValue) {
+      this.errorDuringRequest = null
+      this.meetup = null;
+      this.handleRequest(newValue)
+    }
+  },
+  methods: {
+    async handleRequest(id) {
+      try {
+        this.meetup = await fetchMeetupById(id);
+      } catch (error) {
+        this.errorDuringRequest = error.message
+      }
+    }
+  },
+
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <meetup-view v-if="meetup" :meetup="meetup" ></meetup-view>
 
-      <ui-container>
-        <ui-alert>Загрузка...</ui-alert>
-      </ui-container>
-
-      <ui-container>
-        <ui-alert>error</ui-alert>
+      <ui-container v-else-if="!meetup">
+        <ui-alert v-if="errorDuringRequest">{{ errorDuringRequest }}</ui-alert>
+        <ui-alert v-else>Загрузка...</ui-alert>
       </ui-container>
     </div>`,
 });
